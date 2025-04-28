be8257496513b6b15642d1a7202e17b7283bbe41
nohlsv <36299204+nohlsv@users.noreply.github.com>
2025-04-28 13:32:49 +0800

    Add trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustHosts(at: ['lectica.tech', 'lectica-app-5y8wm.ondigitalocean.app']);
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
