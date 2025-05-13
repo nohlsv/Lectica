a899ba2ecad5e7267cc5efbc715f1f8a85d10db8
nohlsv <36299204+nohlsv@users.noreply.github.com>
2025-05-13 16:09:36 +0800

    Make toast onMounted instead of immediate && gpall

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -2,30 +2,33 @@
 import AppLayout from '@/layouts/app/AppHeaderLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
 import { usePage } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
 import { Toaster } from 'vue-sonner';
 import { toast } from 'vue-sonner';
 
 const page = usePage();
 
-if (page.props.toast) {
-    toast(page.props.toast);
-}
-if (page.props.success) {
-    toast.success(page.props.success);
-}
-if (page.props.error) {
-    toast.error(page.props.error);
-}
-if (page.props.warning) {
-    toast.warning(page.props.warning);
-}
-if (page.props.info) {
-    toast.info(page.props.info);
-}
-if (page.props.message) {
-    toast.info(page.props.message);
-}
-(page.props);
+onMounted(() => {
+    if (page.props.toast) {
+        toast(page.props.toast);
+    }
+    if (page.props.success) {
+        toast.success(page.props.success);
+    }
+    if (page.props.error) {
+        toast.error(page.props.error);
+    }
+    if (page.props.warning) {
+        toast.warning(page.props.warning);
+    }
+    if (page.props.info) {
+        toast.info(page.props.info);
+    }
+    if (page.props.message) {
+        toast.info(page.props.message);
+    }
+});
+
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
