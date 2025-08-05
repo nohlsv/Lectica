6d2d6e69774699dd2d7893e985bd15852dbefbac
tebsss17 <137368444+tebsss17@users.noreply.github.com>
2025-08-05 12:59:09 +0800

    main font change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,6 +32,10 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
