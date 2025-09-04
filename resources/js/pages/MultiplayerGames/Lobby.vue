ea9cf34745641a69e47b3e74b64bd906cb650763
nohlsv <36299204+nohlsv@users.noreply.github.com>
2025-09-05 06:26:14 +0800

    Dont route just tab switch

--- resources/js/pages/MultiplayerGames/Lobby.vue
+++ resources/js/pages/MultiplayerGames/Lobby.vue
@@ -151,12 +151,12 @@
                                 No one has created a multiplayer game yet. Be the first to start a battle!
                             </p>
                             <div class="mt-6">
-                                <Link
-                                    :href="route('multiplayer-games.create')"
+                                <button
+                                    @click="activeTab = 'create'"
                                     class="inline-flex items-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-purple-700 focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:outline-none dark:focus:ring-offset-gray-800"
                                 >
                                     Create First Game
-                                </Link>
+                                </button>
                             </div>
                         </div>
 
