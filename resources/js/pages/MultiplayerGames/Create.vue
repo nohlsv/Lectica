e156eb12c6a46ef1767ae87d0eb3d7ea241bb737
nohlsv <36299204+nohlsv@users.noreply.github.com>
2025-09-05 07:48:59 +0800

    Fixed Joining

--- resources/js/pages/MultiplayerGames/Create.vue
+++ resources/js/pages/MultiplayerGames/Create.vue
@@ -10,6 +10,30 @@
             <div class="mx-auto max-w-4xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <!-- General Error Display -->
+                        <div v-if="form.errors.general || Object.keys(form.errors).length > 0" class="mb-6 rounded-md border border-red-200 bg-red-50 p-4 dark:border-red-800 dark:bg-red-900/20">
+                            <div class="flex">
+                                <div class="flex-shrink-0">
+                                    <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor">
+                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                                <div class="ml-3">
+                                    <h3 class="text-sm font-medium text-red-800 dark:text-red-200">
+                                        There were errors with your submission
+                                    </h3>
+                                    <div class="mt-2 text-sm text-red-700 dark:text-red-300">
+                                        <ul class="list-disc list-inside space-y-1">
+                                            <li v-if="form.errors.general">{{ form.errors.general }}</li>
+                                            <li v-for="(error, field) in form.errors" :key="field" v-if="field !== 'general'">
+                                                <strong>{{ field }}:</strong> {{ error }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         <form @submit.prevent="submit">
                             <!-- Game Mode Selection -->
                             <div class="mb-6">
