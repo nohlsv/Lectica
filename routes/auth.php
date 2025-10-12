8f2efa6cfa0a9d7e73f0863dbb3eae6ae026003f
nohlsv <36299204+nohlsv@users.noreply.github.com>
2025-10-12 16:23:28 +0800

    Quick Fix

--- routes/auth.php
+++ routes/auth.php
@@ -52,9 +52,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
-    Route::delete('profile', [App\Http\Controllers\Settings\ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
-
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
